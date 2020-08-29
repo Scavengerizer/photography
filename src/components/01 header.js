ccafc1015ec7a4d6ec366df89e34272c52941188
Scavengerizer <himanshuthesuperb@gmail.com>
2020-08-29 13:09:23 +0530

    completed header

--- src/components/01 header.js
+++ src/components/01 header.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 function Header() {
-  return <header></header>;
+  return (
+    <header>
+      <div className='header_contents col center'>
+        <div className='logo'>
+          <span>Photography</span>
+        </div>
+        <div className='nav-links row'>
+          <a href='#' className='link'>
+            Home
+          </a>
+          <a href='#' className='link'>
+            Work
+          </a>
+          <a href='#' className='link'>
+            Contact Us
+          </a>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
